eb3594b192409c795f049899553b8e4a91b93566
Koharu2568 <vponsuwan@gmail.com>
2023-10-13 16:04:52 +0700

    add tablist

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import StatusListBox from '../components/StatusListBox.vue'
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+import UserBookmarkPanel from '../components/UserBookmarkPanel.vue';
 import { HeartIcon, PencilIcon } from '@heroicons/vue/20/solid'
 import { ref } from 'vue'
 import { useUserStore } from '../stores/userStore';
@@ -15,8 +16,6 @@ const accountDetail = ref({
 accountDetail.value = {
     ...userStore.information
 }
-
-console.log(userStore)
 
 </script>
 
@@ -62,28 +61,21 @@ console.log(userStore)
                     <p class="text-neutral-400">{{ userStore.getStatusCount('On Hold') }}</p>
                 </div>
             </div>
-            <div class="text-neutral-50 rounded-md mt-8 p-4 md:p-6">
-                <p class="font-medium text-2xl">Bookmark List</p>
-                <div v-if="userStore.watchlist.length" class="flex flex-col gap-4 mt-2">
-                    <div v-for="(item, key) in userStore.watchlist" :key="key"
-                        class="flex items-center p-4 h-auto md:h-20 border border-neutral-700 rounded-md">
-                        <img :src="item.data.images.webp.image_url" alt=""
-                            class="self-start w-24 md:w-auto md:h-full shrink-0 object-cover">
-                        <div class="grid grid-cols-1 md:grid-cols-4 items-center w-full gap-1 justify-center p-4">
-                            <p class="font-medium truncate md:col-span-2">{{ item.data.title }}</p>
-                            <StatusListBox :id="item.id" />
-                            <div class="flex gap-x-2 items-center">
-                                <p class="font-semibold text-2xl">{{ userStore.getProgress(item.id) }}</p>
-                                <p class="text-neutral-400 font-medium">episode watched</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div v-else>
-                    <p class="text-neutral-600 font-semibold text-lg">You don't have any bookmark. Search for anime and try
-                        to bookmark something. :3</p>
-                </div>
-            </div>
+
+            <TabGroup>
+                <TabList class="mt-8 flex">
+                    <Tab class="p-4 text-neutral-50 border-b border-neutral-900 hover:border-green-500">Bookmark</Tab>
+                    <Tab class="p-4 text-neutral-50 border-b border-neutral-900 hover:border-green-500">Ranking</Tab>
+                    <Tab class="p-4 text-neutral-50 border-b border-neutral-900 hover:border-green-500">Tab 3</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <UserBookmarkPanel />
+                    </TabPanel>
+                    <TabPanel>Content 2</TabPanel>
+                    <TabPanel>Content 3</TabPanel>
+                </TabPanels>
+            </TabGroup>
         </div>
     </div>
 </template>
