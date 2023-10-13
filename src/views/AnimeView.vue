a88a852f782f860deaf0121df34ef649794e9f8a
Koharu2568 <vponsuwan@gmail.com>
2023-10-13 15:28:32 +0700

    add characters list

--- src/views/AnimeView.vue
+++ src/views/AnimeView.vue
@@ -2,7 +2,7 @@
 import AnimeDetail from '../components/AnimeDetail.vue';
 import { StarIcon, ClockIcon, CalendarIcon, BookmarkIcon } from '@heroicons/vue/20/solid';
 import { useRoute } from 'vue-router'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import axios from 'axios';
 import { useUserStore } from '../stores/userStore';
 
@@ -11,12 +11,25 @@ const userStore = useUserStore()
 const route = useRoute()
 const id = route.params.id
 const detail = ref()
+const characters = ref()
+
+const sortedCharacters = computed(() => {
+    const charactersCpy = characters.value
+    console.log(charactersCpy)
+    if (charactersCpy) {
+        charactersCpy.sort((a, b) => b.favorites - a.favorites);
+    }
+    return charactersCpy
+})
+const currentLoadCharaNumber = ref(12)
 
 const baseURL = 'https://api.jikan.moe/v4/'
 const loadDetail = async () => {
-    const response = await axios.get('anime/' + id + '/full', { baseURL: baseURL })
-    detail.value = response.data.data
-    console.log(response.data.data)
+    const a_response = await axios.get('anime/' + id + '/full', { baseURL: baseURL })
+    detail.value = a_response.data.data
+    const c_response = await axios.get('anime/' + id + '/characters', { baseURL: baseURL })
+    characters.value = c_response.data.data
+    console.log(c_response.data.data)
 }
 
 loadDetail()
@@ -26,6 +39,10 @@ function addToList() {
         ...detail.value
     }
     userStore.addWatchList(data)
+}
+
+function loadMoreChara() {
+    currentLoadCharaNumber.value += 20;
 }
 
 </script>
@@ -72,6 +89,22 @@ function addToList() {
                 <p class="text-neutral-600">source myanimelist</p>
             </div>
         </div>
-        <AnimeDetail :detail="detail" />
+        <AnimeDetail v-if="detail" :detail="detail" />
+        <div v-if="sortedCharacters" class="flex flex-col w-full p-4 md:p-8 border border-neutral-800 rounded-md mt-8">
+            <p class="text-xl font-base text-neutral-400">Characters ({{ sortedCharacters.length }}) </p>
+            <hr class="mt-4 mb-4 bg-neutral-800 border-0 h-px">
+            <div class="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-2 md:gap-8 text-neutral-100">
+                <div class="flex flex-col items-center rounded-md border border-neutral-800 p-2"
+                    v-for="(chara, index) in sortedCharacters.slice(0, currentLoadCharaNumber)" :key="index">
+                    <img :src="chara.character.images.webp.image_url" alt="" class="object-contain w-20 md:w-auto">
+                    <p class="text-center font-medium">{{ chara.character.name }}</p>
+                    <p class="text-center text-neutral-400" v-if="chara.voice_actors.length">{{
+                        chara.voice_actors[0].person.name }}</p>
+                </div>
+            </div>
+            <button @click="loadMoreChara" v-if="currentLoadCharaNumber <= sortedCharacters.length"
+                class="px-4 py-2 mt-4 bg-green-600 text-neutral-100 rounded-md w-fit mx-auto font-medium">Show
+                More</button>
+        </div>
     </div>
 </template>
