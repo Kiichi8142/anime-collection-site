c2dcf80c8767cd3209c70f8bd8a675e2a11be07e
Koharu2568 <vponsuwan@gmail.com>
2023-10-15 12:37:02 +0700

    add upcoming, current season

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -3,9 +3,10 @@ import AnimeCard from '../components/AnimeCard.vue';
 
 import axios from 'axios';
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useAnimeStore } from '@/stores/animeStore.js'
 
+const router = useRouter()
 const route = useRoute()
 const type = route.params.type
 
@@ -13,6 +14,8 @@ const animeStore = useAnimeStore()
 
 const baseURL = 'https://api.jikan.moe/v4/'
 const data = ref()
+const season = ref()
+const upcoming = ref()
 
 const loadTop = async () => {
     const response = await axios.get('top/' + type, { baseURL: baseURL })
@@ -21,11 +24,37 @@ const loadTop = async () => {
     console.log(data.value)
 }
 
+const loadSeason = async () => {
+    const response = await axios.get('seasons/now', { baseURL: baseURL })
+    season.value = response.data
+    animeStore.season.current = response.data
+    console.log(season.value)
+}
+
+const loadUpcoming = async () => {
+    const response = await axios.get('seasons/upcoming', { baseURL: baseURL })
+    upcoming.value = response.data
+    animeStore.season.upcoming = response.data
+    console.log(season.value)
+}
+
 onMounted(() => {
     if (animeStore.top[type]) {
         data.value = animeStore.top[type]
     } else {
         loadTop()
+    }
+
+    if (animeStore.season.current) {
+        season.value = animeStore.season.current
+    } else {
+        loadSeason()
+    }
+
+    if (animeStore.season.upcoming) {
+        upcoming.value = animeStore.season.upcoming
+    } else {
+        loadUpcoming()
     }
 })
 
@@ -33,20 +62,40 @@ onMounted(() => {
 
 <template>
     <div class="max-w-7xl mx-auto text-neutral-50 p-6 lg:p-8">
-        <h1 class="font-bold text-3xl">All time popular</h1>
         <div v-if="data">
-            <div v-if="type != 'characters'"
-                class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-content-center mt-4 max-w-7xl">
-                <div v-for="(anime, key) in data.data.slice(0, 4)" v-bind:key="key">
-                    <AnimeCard :title="anime.title" :img-url="anime.images.webp.large_image_url"
-                        :title-english="anime.title_english" :id="anime.mal_id" :type="anime.type" />
-                </div>
-            </div>
-            <div v-else
+            <h1 class="font-bold text-3xl">All time popular</h1>
+            <div
                 class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-content-center mt-4 max-w-7xl overflow-x-auto">
                 <div v-for="(chara, key) in data.data.slice(0, 4)" v-bind:key="key">
-                    <AnimeCard :title="chara.name" :img-url="chara.images.webp.image_url" :title-english="chara.name_kanji"
-                        type="characters" />
+                    <AnimeCard :title="chara.name" :img-url="chara.images.webp.large_image_url"
+                        :title-english="chara.name_kanji" type="characters" />
+                </div>
+            </div>
+        </div>
+        <div v-if="season" class="mt-8">
+            <div class="flex items-end justify-between">
+                <h1 class="font-bold text-3xl">Current Season</h1>
+                <button @click="router.push({ name: 'seasonal' })" class="text-neutral-400">View All</button>
+            </div>
+            <div
+                class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-content-center mt-4 max-w-7xl overflow-x-auto">
+                <div v-for="(chara, key) in season.data.slice(0, 4)" v-bind:key="key">
+                    <AnimeCard :title="chara.name" :img-url="chara.images.webp.large_image_url"
+                        :title-english="chara.name_kanji" type="characters" />
+                </div>
+            </div>
+        </div>
+
+        <div v-if="upcoming" class="mt-8">
+            <div class="flex items-end justify-between">
+                <h1 class="font-bold text-3xl">Upcoming</h1>
+                <button class="text-neutral-400">View All</button>
+            </div>
+            <div
+                class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-content-center mt-4 max-w-7xl overflow-x-auto">
+                <div v-for="(chara, key) in upcoming.data.slice(0, 4)" v-bind:key="key">
+                    <AnimeCard :title="chara.name" :img-url="chara.images.webp.large_image_url"
+                        :title-english="chara.name_kanji" type="characters" />
                 </div>
             </div>
         </div>
