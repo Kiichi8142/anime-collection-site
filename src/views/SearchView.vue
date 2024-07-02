4097c172789e3178a1336d5f5fae9ff6f92cc85a
Kiichi8142 <vponsuwan@gmail.com>
2024-07-03 01:11:14 +0700

    add filter options

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -8,15 +8,26 @@ const AnimeGridCard = defineAsyncComponent(
 );
 
 const types = [
-  { text: 'TV' },
-  { text: 'Movie' },
-  { text: 'OVA' },
-  { text: 'Special' },
-  { text: 'ONA' },
-  { text: 'Music' },
+  { text: 'TV', value: 'tv' },
+  { text: 'Movie', value: 'movie' },
+  { text: 'OVA', value: 'ova' },
+  { text: 'Special', value: 'special' },
+  { text: 'ONA', value: 'ona' },
 ]
 
-const selectedType = ref('TV')
+const selectedType = ref(types[0].value)
+
+const ratings = [
+  { text: 'None', value: '' },
+  { text: 'G - All Ages', value: 'g' },
+  { text: 'PG - Children', value: 'pg' },
+  { text: 'PG-13 - Teens 13 or older', value: 'pg13' },
+  { text: 'R - 17+ (violence & profanity)', value: 'r17' },
+  { text: 'R+ - Mild Nudity', value: 'r' },
+  { text: 'Rx - Hentai', value: 'rx' },
+]
+
+const selectedRating = ref(ratings[0].value)
 
 const route = useRoute()
 const router = useRouter()
@@ -38,30 +49,45 @@ const prevPage = async () => {
 }
 
 const fetchSearch = async (page) => {
-  const searchResponse = await animeSearch(searchValue.value, selectedType.value, page)
+  const searchResponse = await animeSearch(searchValue.value, selectedType.value, page, selectedRating.value)
   searchDetails.value = searchResponse.data.data
   pagesData.value = searchResponse.data.pagination
 }
 
 watchEffect(async () => {
   await fetchSearch(route.query?.page || 1);
-  router.push({ query: { q: searchValue.value, page: pagesData.value?.current_page } })
+  router.push({ query: { q: searchValue.value, type: selectedType.value, page: pagesData.value?.current_page, rating: selectedRating.value } })
 })
 
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto p-4 lg:p-8">
-    <div class="flex space-x-2">
+    <div class="flex flex-col space-y-2">
       <h1 class="font-medium text-4xl text-gray-100">Search</h1>
-      <select class="bg-neutral-800 p-1 rounded-md font-sm w-fit text-neutral-50" v-model="selectedType">
-        <option v-for="(type, typeIndex) in types" :key="typeIndex" :value="type.text">
-          {{ type.text }}
-        </option>
-      </select>
+      <input placeholder="Type in keywords..." v-model.lazy="searchValue"
+        class="w-full mt-4 text-gray-100 p-2 bg-neutral-800 rounded-lg placeholder-neutral-600" type="text">
+      <div class="flex space-x-2">
+        <div class="space-x-1">
+          <label for="type" class="text-neutral-50 font-medium">Type</label>
+          <select name="type" class="bg-neutral-800 p-1 rounded-md font-sm w-fit text-neutral-50"
+            v-model="selectedType">
+            <option v-for="(type, typeIndex) in types" :key="typeIndex" :value="type.value">
+              {{ type.text }}
+            </option>
+          </select>
+        </div>
+        <div class="space-x-1">
+          <label for="type" class="text-neutral-50 font-medium">Rating Filter</label>
+          <select name="type" class="bg-neutral-800 p-1 rounded-md font-sm w-fit text-neutral-50"
+            v-model="selectedRating">
+            <option v-for="(rate, rateIndex) in ratings" :key="rateIndex" :value="rate.value">
+              {{ rate.text }}
+            </option>
+          </select>
+        </div>
+      </div>
     </div>
-    <input placeholder="Type here to search" v-model.lazy="searchValue"
-      class="w-full mt-4 text-gray-100 p-2 bg-neutral-800 rounded-lg placeholder-neutral-600" type="text">
     <div v-if="pagesData && searchDetails">
       <AnimeGridCard :anime-data="searchDetails" :page-data="pagesData" @next-page="nextPage" @prev-page="prevPage" />
     </div>
